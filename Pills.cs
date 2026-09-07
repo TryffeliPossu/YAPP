4d4c8df4819503cc196d854813bfa82064938b05
TryffeliPossu <jed.juutinen@proton.me>
2026-09-07 17:17:25 +0300

    Update Pills.cs

--- Pills.cs
+++ Pills.cs
@@ -27,8 +27,9 @@ namespace YAPP.Pills
 
         public override void OnUsed(PlayerUsedItemEventArgs ev)
         {
-            ev.Player.EnableEffect<MovementBoost>(200, 7.5f);
+            ev.Player.EnableEffect<MovementBoost>(200, 7f);
             ev.Player.EnableEffect<Invigorated>(1, 5f);
+            ev.Player.EnableEffect<Deafened>(1, 7f);
         }
     }
     
